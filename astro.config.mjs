b4d4193bacffd2ec26ceb97ffec35a3297fbdd2d
Frederick Chyan <fchyan@gmail.com>
2025-07-27 11:07:39 -0700

    Finished Tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fredchyan.github.io',
+  site: 'https://fredchyan.github.io',
+  integrations: [preact()],
 });
